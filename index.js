b0cf6061ca092969529b2aacf49c30f256c83781
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-10-10 13:13:02 +0300

    Completed Assigment

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // Convert the word to lowercase to make the comparison case-insensitive
+  const lowercasedWord = word.toLowerCase();
+
+  // Initialize pointers for the start and end of the word
+  let start = 0;
+  let end = lowercasedWord.length - 1;
+
+  // Iterate until the start pointer is less than the end pointer
+  while (start < end) {
+    // If characters at the start and end pointers are not equal, the word is not a palindrome
+    if (lowercasedWord[start] !== lowercasedWord[end]) {
+      return false;
+    }
+
+    // Move the pointers towards each other
+    start++;
+    end--;
+  }
+
+  // If the loop completes, the word is a palindrome
+  return true;
 }
 
 /* 
-  Add your pseudocode here
-*/
+  Pseudocode:
 
-/*
-  Add written explanation of your solution here
+  1. Convert the input word to lowercase to make the comparison case-insensitive.
+  2. Initialize two pointers, one at the start of the word and the other at the end.
+  3. While the start pointer is less than the end pointer:
+     a. If the characters at the start and end pointers are not equal, return false (not a palindrome).
+     b. Move the start pointer forward and the end pointer backward.
+  4. If the loop completes, return true (the word is a palindrome).
+
+  Explanation:
+
+  The function uses two pointers starting from the beginning and end of the word and compares the characters at these positions. If they are not equal at any point, the word is not a palindrome. The pointers move towards each other until they meet or cross, and if the loop completes, the word is a palindrome.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  // Add your own custom tests here
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
@@ -23,3 +50,4 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
